5816ed983192cc75ac24451203f8299d6f2da7b2
Michael-Reis <michael_farias14@hotmail.com>
2023-06-03 23:01:09 -0300

    navegação

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,1 +1,21 @@
-// comentario
+import { StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
+
+
+
+export default function Produtos() {
+    return (
+        <View style={styles.container}>
+            <Text>Produtos</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: '#ff5b00',
+    }
+});
