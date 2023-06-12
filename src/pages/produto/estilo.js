318f91e7e780ba84eeac14eb94e8347a0d545cec
Michael Reis <michael.reis@orit.com.br>
2023-06-12 14:23:10 -0300

    subindo

--- src/pages/produto/estilo.js
+++ src/pages/produto/estilo.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 import { Dimensions } from 'react-native';
 
 export const styles = StyleSheet.create({
-    // comentarios
+    // comentarios2
     container: {
         flex: 1,
         backgroundColor: '#fff',
